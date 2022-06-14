9fe8d7fb7ac3633202ceef435e4d47acb0eafdf1
Udith Chandrarathna <udithsc@gmail.com>
2022-06-14 08:49:35 +0530

    shopping cart integration

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <p>2022 USD Store All rights reserved</p>
+      <p>2022 UDT Store All rights reserved</p>
       <p className="icons">
         <AiFillInstagram />
         <AiOutlineTwitter />
--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Toaster } from 'react-hot-toast';
 
 import { Layout } from '../components';
+import { StateContext } from '../context/StateContext';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Toaster />
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   );
 }
 
