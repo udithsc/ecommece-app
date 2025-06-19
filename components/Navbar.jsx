7099bf24101057b9ef136519b9f8a015dcc2d9cb
Udith Chandrarathna <udithsc@gmail.com>
2025-06-20 02:27:48 +0800

    navbar updated

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
-import { AiOutlineShopping } from 'react-icons/ai';
+import { AiOutlineShopping, AiOutlineUser } from 'react-icons/ai';
+import Image from 'next/image';
 
 import { Cart } from './';
 import { useStateContext } from '../context/StateContext';
@@ -9,20 +10,59 @@ const Navbar = () => {
   const { showCart, setShowCart, totalQuantities } = useStateContext();
 
   return (
-    <div className="navbar-container">
-      <p className="logo">
-        <Link href="/">UDT Store</Link>
-      </p>
-
-      <button
-        type="button"
-        className="cart-icon"
-        onClick={() => setShowCart(true)}
-      >
-        <AiOutlineShopping />
-        <span className="cart-item-qty">{totalQuantities}</span>
-      </button>
-
+    <div className='navbar-container'>
+      <div className='navbar-inner'>
+        <div className='navbar-left'>
+          <Link href='/'>
+            <a className='logo-image-link'>
+              <Image
+                src='/udt-store-logo.png'
+                alt='UDT Store Logo'
+                width={100}
+                height={60}
+                className='logo-image'
+              />
+            </a>
+          </Link>
+        </div>
+        <nav className='navbar-center'>
+          <Link href='/'>
+            <a className='menu-item'>Mac</a>
+          </Link>
+          <Link href='/'>
+            <a className='menu-item'>iPad</a>
+          </Link>
+          <Link href='/'>
+            <a className='menu-item'>iPhone</a>
+          </Link>
+          <Link href='/'>
+            <a className='menu-item'>Watch</a>
+          </Link>
+          <Link href='/'>
+            <a className='menu-item'>AirPods</a>
+          </Link>
+          <Link href='/'>
+            <a className='menu-item'>TV & Home</a>
+          </Link>
+          <Link href='/'>
+            <a className='menu-item'>Accessories</a>
+          </Link>
+          <Link href='/'>
+            <a className='menu-item'>Offer</a>
+          </Link>
+        </nav>
+        <div className='navbar-right'>
+          <button
+            type='button'
+            className='cart-icon'
+            onClick={() => setShowCart(true)}
+          >
+            <AiOutlineShopping />
+            <span className='cart-item-qty'>{totalQuantities}</span>
+          </button>
+          <AiOutlineUser className='user-icon' />
+        </div>
+      </div>
       {showCart && <Cart />}
     </div>
   );
